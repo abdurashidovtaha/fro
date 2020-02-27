bfe710bf591f0a718731ee0ab47258ed0f1595d1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-27 08:54:39 +0000

    Deploy abdurashidovtaha/fro to github.com/abdurashidovtaha/fro.git:gh-pages

---
+++ precache-manifest.506ba45b381ddb97729686013124e2f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d52b861aafa9491990aab5673ba469a4",
+    "url": "/fro/index.html"
+  },
+  {
+    "revision": "4c719d64c5f877339298",
+    "url": "/fro/static/css/main.47556096.chunk.css"
+  },
+  {
+    "revision": "d13343a6e828c410daf1",
+    "url": "/fro/static/js/2.03b10169.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/fro/static/js/2.03b10169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c719d64c5f877339298",
+    "url": "/fro/static/js/main.1bcbd85b.chunk.js"
+  },
+  {
+    "revision": "8d345cf322cef0827245",
+    "url": "/fro/static/js/runtime-main.5b11a0fd.js"
+  }
+]);
